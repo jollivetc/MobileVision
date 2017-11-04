65c4122f907f243ada1e546104a05e05fcdc8549
Christophe Jollivet <christophe.jollivet@gmail.com>
2017-11-04 15:48:48 +0100

    add FaceTracker final project

---
+++ FaceTracker/app/src/main/java/fr/jollivetc/facetracker/FaceGraphic.java
@@ -0,0 +1,108 @@
+package fr.jollivetc.facetracker;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Size;
+
+import com.google.android.gms.samples.vision.face.facetracker.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.Landmark;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handle the drawing of the mask on the overlay.
+ */
+class FaceGraphic extends GraphicOverlay.Graphic {
+
+    private final static int TEMPLATE_INTER_EYES_DISTANCE = 60;
+    private final static int TEMPLATE_WIDTH = 310;
+    private final static int TEMPLATE_HEIGHT = 310;
+    private volatile Face mFace;
+    private Drawable drawable;
+    private Resources resources;
+    private Map<Integer, PointF> previousLandmark = new HashMap<>();
+
+    FaceGraphic(GraphicOverlay overlay, Resources resources) {
+        super(overlay);
+        drawable = resources.getDrawable(R.drawable.captain);
+        this.resources = resources;
+    }
+
+
+    /**
+     * Updates the face instance from the detection of the most recent frame.  Invalidates the
+     * relevant portions of the overlay to trigger a redraw.
+     */
+    void updateFace(Face face) {
+        mFace = face;
+        postInvalidate();
+    }
+
+    /**
+     * Compute the position and size of the face to adapt the mask and draw it on the overlay.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        PointF detectLeftPosition = getLandmark(Landmark.LEFT_EYE);
+        PointF detectRightPosition = getLandmark(Landmark.RIGHT_EYE);
+        if ((detectLeftPosition == null) || (detectRightPosition == null)) {
+            return;
+        }
+
+        PointF leftEyePosition =
+                new PointF(translateX(detectLeftPosition.x), translateY(detectLeftPosition.y));
+        PointF rightEyePosition =
+                new PointF(translateX(detectRightPosition.x), translateY(detectRightPosition.y));
+
+        double rotationAngle = Math.atan((leftEyePosition.y-rightEyePosition.y)/(leftEyePosition.x-rightEyePosition.x));
+        float eyesDistance = (float)(Math.sqrt(Math.pow((rightEyePosition.x-leftEyePosition.x), 2)+ Math.pow((rightEyePosition.y - leftEyePosition.y), 2)));
+        float resizeFactor = (eyesDistance)/ TEMPLATE_INTER_EYES_DISTANCE;
+
+        PointF faceCenter = new PointF(leftEyePosition.x + (rightEyePosition.x - leftEyePosition.x) / 2, leftEyePosition.y + (rightEyePosition.y - leftEyePosition.y) / 2);
+        Size finalDrawableSize = computeNewDrawableSize(resizeFactor, Math.abs(rotationAngle));
+        Drawable drawableToDraw = rotateDrawable(rotationAngle);
+
+        float top = faceCenter.y - finalDrawableSize.getHeight()/2;
+        float left = faceCenter.x- finalDrawableSize.getWidth()/2;
+        float right = faceCenter.x+finalDrawableSize.getWidth()/2;
+        float bottom = faceCenter.y+ finalDrawableSize.getHeight()/2;
+
+        drawableToDraw.setBounds(Math.round(left), Math.round(top), Math.round(right), Math.round(bottom));
+        drawableToDraw.draw(canvas);
+    }
+
+    private PointF getLandmark(int landMarkType){
+        for (Landmark landmark : mFace.getLandmarks()) {
+            if (landmark.getType() == landMarkType) {
+                previousLandmark.put(landMarkType, landmark.getPosition());
+                return landmark.getPosition();
+            }
+        }
+        return previousLandmark.get(landMarkType);
+    }
+
+    private Drawable rotateDrawable(double rotationAngle) {
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        Matrix matrix = new Matrix();
+        matrix.postRotate((float)(Math.toDegrees(rotationAngle)));
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap,bitmap.getWidth(),bitmap.getHeight(),true);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap , 0, 0, scaledBitmap .getWidth(), scaledBitmap .getHeight(), matrix, true);
+        return new BitmapDrawable(resources, rotatedBitmap);
+    }
+
+    private Size computeNewDrawableSize(float resizeFactor, double rotationAngle){
+        float resizeWidth = TEMPLATE_WIDTH * resizeFactor;
+        float resizeHeight = TEMPLATE_HEIGHT * resizeFactor;
+        float finalWidth = (float) (resizeWidth * Math.cos(rotationAngle) + resizeHeight * Math.sin(rotationAngle));
+        float finalHeight = (float) (resizeWidth * Math.sin(rotationAngle) + resizeHeight * Math.cos(rotationAngle));
+
+        return new Size(Math.round(finalWidth), Math.round(finalHeight));
+    }
+}
---
+++ FaceTracker/app/src/main/java/fr/jollivetc/facetracker/MainActivity.java
@@ -0,0 +1,164 @@
+package fr.jollivetc.facetracker;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.samples.vision.face.facetracker.ui.camera.CameraSourcePreview;
+import com.google.android.gms.samples.vision.face.facetracker.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "FaceTracker";
+
+    private CameraSource cameraSource = null;
+
+    private CameraSourcePreview preview;
+    private GraphicOverlay graphicOverlay;
+
+    private static final int RC_HANDLE_GMS = 9001;
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        preview = (CameraSourcePreview) findViewById(R.id.preview);
+        graphicOverlay = (GraphicOverlay) findViewById(R.id.faceOverlay);
+
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            createCameraSource();
+        }else{
+            requestCameraPermission();
+        }
+    }
+
+    private void createCameraSource() {
+        Context context = getApplicationContext();
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        detector.setProcessor(
+                new MultiProcessor.Builder<>(new MultiProcessor.Factory<Face>() {
+                    @Override
+                    public Tracker<Face> create(Face face) {
+                        return new GraphicFaceTracker(graphicOverlay, getResources());
+                    }
+                }).build());
+
+        if (!detector.isOperational()) {
+            Log.w(TAG, "Face detector dependencies are not yet available.");
+        }
+        cameraSource = new CameraSource.Builder(context, detector)
+                .setRequestedPreviewSize(640, 480)
+                .setFacing(CameraSource.CAMERA_FACING_FRONT)
+                .setRequestedFps(30.0f)
+                .build();
+    }
+
+    private void requestCameraPermission() {
+        Log.w(TAG, "Camera permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+        Toast.makeText(getApplicationContext(), "Access to camera is needed", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startCameraSource();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        preview.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraSource != null) {
+            cameraSource.release();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode != RC_HANDLE_CAMERA_PERM) {
+            Log.d(TAG, "Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Camera permission granted - initialize the camera source");
+            createCameraSource();
+            return;
+        }
+
+        Log.e(TAG, "Permission not granted: results len = " + grantResults.length +
+                " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Face Tracker sample")
+                .setMessage(R.string.no_camera_permission)
+                .setPositiveButton(R.string.ok, listener)
+                .show();
+    }
+
+    private void startCameraSource() {
+        // check that the device has play services available.
+        int code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(
+                getApplicationContext());
+        if (code != ConnectionResult.SUCCESS) {
+            Dialog dlg =
+                    GoogleApiAvailability.getInstance().getErrorDialog(this, code, RC_HANDLE_GMS);
+            dlg.show();
+        }
+
+        if (cameraSource != null) {
+            try {
+                preview.start(cameraSource, graphicOverlay);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to start camera source.", e);
+                cameraSource.release();
+                cameraSource = null;
+            }
+        }
+    }
+
+}
